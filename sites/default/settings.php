692f3a0745ffd7e2b92119b168b4f3307c414d3b
Jeremy.Sloan <jeremy.sloan@webfilings.com>
2015-04-07 13:00:08 -0500

    Update settings.php file

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -29,8 +29,8 @@ ini_set('url_rewriter.tags',        '');
 // database in each site environment (Dev, Stage, or Prod). To use this 
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-  require '/var/www/site-php/dccommunitysplashpad/dccommunitysplashpad-settings.inc';
+if (file_exists('/var/zpanel/hostdata/jeremysloan/www_config/dccommunitysplashpad-settings.inc')) {
+  require('/var/zpanel/hostdata/jeremysloan/www_config/dccommunitysplashpad-settings.inc');
 }
 
 if (file_exists('/mnt/webdev/configs/dcsplashpad/dcsplashpad.com.inc')) {
